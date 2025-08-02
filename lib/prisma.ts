c8c2c52db1e5df5a9168ce9c405bb234bd8fa101
aamirskhan <aamir@coretechies.com>
2025-08-02 07:12:13 +0530

    Initial Commit 17

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,11 +8,13 @@ export const prisma =
     log:
       process.env.NODE_ENV === 'development'
         ? ['query', 'info', 'warn', 'error']
-        : [],
+        : ["error"],
   });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
+
+
 
 export default prisma;
